3f7e7c803d595aaed514bc2f1a363917e170b7aa
Jason Blanchard <jas.blanchard@gmail.com>
2014-10-02 08:00:25 -0400

    Made more specific grunt task

--- ember_pipeline_client/Gruntfile.js
+++ ember_pipeline_client/Gruntfile.js
@@ -3,17 +3,23 @@ module.exports = function(grunt) {
   grunt.initConfig({
     redis: {
       options: {
-        prefix:'dev:',
-        host: '192.168.59.103'
+        prefix:'',
+        host: ''
       },
-      default: {
+      dev: {
+        options: {
+          prefix:'dev:',
+          host: '192.168.59.103'
+        },
         files: {
           src: ["dist/index.html"]
         }
-      }
+      },
     }
   });
 
   grunt.loadNpmTasks('grunt-redis');
+  
+  grunt.registerTask('deploy:dev', ['redis:dev']);
 
 };
