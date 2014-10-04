006dae2893b6b2550aa76f396308cd92f1458ec6
Jason Blanchard <jas.blanchard@gmail.com>
2014-10-04 09:52:18 -0400

    Fixed ember-cli and installed simple auth devise

---
+++ ember_pipeline_client/app/views/controllers/posts/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  post: function() {
+    return this.store.createRecord('post');
+  }.property(),
+
+  actions: {
+    createPost: function() {
+      var self = this;
+
+      self.get('post').save().then(function() {
+        self.transitionToRoute('posts.index');
+      });
+    }
+  }
+
+});
